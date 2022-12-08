5c6f95a154563fc8a82de89e3051f29bc12adaf4
Conner Grice <82033688+ConnerGrice@users.noreply.github.com>
2022-12-08 00:04:57 +0000

    Finish day5

--- 5/day5.py
+++ 5/day5.py
@@ -1,8 +1,70 @@
+import re
+import copy
+
 class CrateStack:
 
     def __init__(self,crates):
-        self.crates = crates
-        self.top = crates[-1]
+        self.crates = crates[::-1]
+        self.top = self.crates[-1]
+
+    def add(self,crate):
+        """
+        Adds a crate to the top of the stack
+        """
+        if isinstance(crate,list):
+            self.crates.extend(crate)
+        else:
+            self.crates.append(crate)
+
+    def updateTop(self):
+        """
+        Updates the top crate value
+        """
+        if self.crates:
+            self.top = self.crates[-1]
+        else:
+            self.top = "_"
+    
+
+    def moveTo(self,other_stack,num_moves):
+        """
+        Moves the top crate from current stack to a given stack
+        """
+
+        #Moves the given number of times
+        for _ in range(num_moves):
+            #Takes crate off the top of given stack
+            crate = self.crates.pop()
+
+            #Adds crate to the top of the other stack
+            other_stack.add(crate)
+        
+        #Updates top of stack
+        other_stack.updateTop()
+        self.updateTop()
+
+
+    def moveMulti(self,other_stack,num_moves):
+        """
+        Moves multiple crates at once, conserving order
+        """
+        #Length of stack
+        stack_len = len(self.crates)
+
+        #Gets a list of the top crates
+        to_move = self.crates[stack_len-num_moves:]
+
+        #Deletes top crates from list
+        del self.crates[stack_len-num_moves:]
+
+        #Add those crates to the other stack
+        other_stack.add(to_move)
+
+        #Updates top cratea
+        other_stack.updateTop()
+        self.updateTop()
+
+    
 
 def findCrates(content):
     """
@@ -46,17 +108,72 @@ def organiseCrates(stack_list):
     return stack_dict
 
 def loadCrates(stacks):
+    """
+    Converts each list of crates into a stack object
+    """
     for item in stacks.items():
         stacks[item[0]] = CrateStack(item[1])
     
     return stacks
 
+def getCommands(content):
+    """
+    Parse puzzle input for the useable command values
+    first - number of moves
+    second - stack to move from
+    third - stack to move to
+    """
+    command_list = []
+    #Loops through the input that does not contain stack config
+    for i in range(10,len(content)):
+        #Removes all non-number characters and splits into a list
+        command = re.sub(r"[a-zA-Z]","",content[i]).split()
+
+        #Converts command values into integers
+        command = [int(x) for x in command]
+
+        command_list.append(command)
+    
+    return command_list
+
+        
+
+
 with open("input.txt","r") as f:
+    #Read entire file
     puzzle = f.readlines()
-    thing = findCrates(puzzle)
-    thing_dict = organiseCrates(thing)
-    thing_dict = loadCrates(thing_dict)
-    print(thing_dict[1].crates)
+
+    #Generates tuples containing each crate position
+    crates = findCrates(puzzle)
+
+    #Puts stack lists into a dict
+    crates_dict = organiseCrates(crates)
+
+    #Converts stack lists into stack objects
+    crates_dict = loadCrates(crates_dict)
+
+    #Gets a copy of the original config for task 2
+    crates_copy = copy.deepcopy(crates_dict)
+
+    #Gets a list of commands
+    commands = getCommands(puzzle)
+
+#Loops through all commands
+for command in commands:
+    #Carries out command
+    crates_dict[command[1]].moveTo(crates_dict[command[2]],command[0]) 
+    crates_copy[command[1]].moveMulti(crates_copy[command[2]],command[0])  
+
+#Prints top crates in final config
+top_crates = ""
+top_copy = ""
+for task1,task2 in zip(crates_dict.values(),crates_copy.values()):
+    top_crates += task1.top
+    top_copy += task2.top
+
+print("Task 1: ",top_crates)
+print("Task 2: ",top_copy)
+
 
 
 
